4da27e590777e37229061a43a6e81ef0d4d0fef3
cheryl <rook2q5@gmail.com>
2020-03-01 11:06:26 -0500

    add books_by_tag.html and update models.py

--- store/models.py
+++ store/models.py
@@ -5,38 +5,61 @@ from django.contrib.auth.models import User
 from django.utils.text import slugify
 
 # Create your models here.
+
+
 class Author(models.Model):
-  first_name = models.CharField(max_length=100)
-  last_name = models.CharField(max_length=100)
-  
-  def __str__(self):
-    return f"{self.last_name}, {self.first_name}"
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
+
 
 class Category(models.Model):
-  title = models.CharField(max_length=100)
-  slug = models.SlugField(null=False, unique=True)
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(null=False, unique=True)
 
-  def __str__(self):
-    return f"{self.title}"
-  
-  def save(self, *args, **kwargs):
+    def __str__(self):
+        return f"{self.title}"
+
+    def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify(self.title)
-        return super().save(*args, **kwargs) 
-  
+        return super().save(*args, **kwargs)
+
+
 class Book(models.Model):
-  title = models.CharField(max_length=100)
-  author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
-  publish_date = models.DateField(default=timezone.now)
-  price = models.DecimalField(decimal_places=2, max_digits=8)
-  description = models.TextField()
-  created_at = models.DateTimeField(auto_now_add=True)
-  category = models.ForeignKey(Category, on_delete=models.DO_NOTHING, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    publish_date = models.DateField(default=timezone.now)
+    price = models.DecimalField(decimal_places=2, max_digits=8)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.DO_NOTHING, null=True, blank=True)
+    tag = models.ForeignKey(
+        'Tag', on_delete=models.DO_NOTHING, null=True, blank=True)
+
+    def __str__(self):
+        return f"Note item: {self.item} description: {self.description}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(null=False, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
+
 
 class Cart(models.Model):
-  user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
-  active = models.BooleanField(default=True)
-  order_date = models.DateField(null=True)
-  payment_type = models.CharField(max_length=100, null=True)
-  payment_id = models.CharField(max_length=100, null=True)
-
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    active = models.BooleanField(default=True)
+    order_date = models.DateField(null=True)
+    payment_type = models.CharField(max_length=100, null=True)
+    payment_id = models.CharField(max_length=100, null=True)
--- store/views.py
+++ store/views.py
@@ -1,30 +1,35 @@
 from django.shortcuts import render
 from django.shortcuts import redirect, get_object_or_404
 from django.http import HttpResponse
-from .models import Book, Cart, Category
+from .models import Book, Cart, Category, Tag
 
 # Create your views here.
 
+
 def index(request):
-  return render(request, 'template.html')
+    return render(request, 'template.html')
+
 
 def store(request):
-  books = Book.objects.order_by('-created_at')
-  return render(request, 'base.html', {'books': books})
+    books = Book.objects.order_by('-created_at')
+    return render(request, 'base.html', {'books': books})
+
 
 def book_details(request, pk):
-  book = get_object_or_404(Book, pk=pk)
-  return render(request, 'store/detail.html', {'book': book})
+    book = get_object_or_404(Book, pk=pk)
+    return render(request, 'store/detail.html', {'book': book})
+
 
 def books_by_category(request, slug):
-  category = Category.objects.get(slug=slug)
-  books_for_category=Book.objects.filter(category=category)
-  return render(request, 'store/books_by_tag.html', {'books':books_for_category, 'category': category })
-  
+    category = Category.objects.get(slug=slug)
+    books_for_category = Book.objects.filter(category=category)
+    return render(request, 'store/books_by_tag.html', {'books': books_for_category, 'category': category})
+
+
 # def cart(request):
 #   if request.user.is_authenticated():
 #     cart = Cart.objects.filter(user=request.user.id, active=True)
-#     
+#
 #     total = 0
 #     count = 0
 #     for order in orders:
