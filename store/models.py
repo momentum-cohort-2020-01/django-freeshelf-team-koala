3fb28e153fe4530a57baa96a5d04cf1576785e61
cheryl <rook2q5@gmail.com>
2020-03-02 07:55:55 -0500

    many updates, incl migration

--- store/models.py
+++ store/models.py
@@ -34,12 +34,22 @@ class Book(models.Model):
     publish_date = models.DateField(default=timezone.now)
     price = models.DecimalField(decimal_places=2, max_digits=8)
     description = models.TextField()
+    reviews = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(
         Category, on_delete=models.DO_NOTHING, null=True, blank=True)
 
     def __str__(self):
-        return f"Note item: {self.item} description: {self.description}"
+        return f'{self.title}'
+
+
+class Image(models.Model):
+    image_name = models.CharField(max_length=400)
+    imagefile = models.FileField(
+        upload_to='images', null=True, verbose_name=None)
+
+    def __str__(self):
+        return f'{self.name} {self.imagefile}'
 
 
 class Cart(models.Model):
